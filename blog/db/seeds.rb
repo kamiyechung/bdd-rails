35019ed19ce2e10833acdc816e293716dc56e812
Jordan Aumis <jordan0aumis@gmail.com>
2019-01-30 14:33:53 +0100

    premier push

--- blog/db/seeds.rb
+++ blog/db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+
+10.times do 
+  user = User.create!(first_name: Faker::OnePiece.character, last_name: Faker::Pokemon.name, email: Faker::Internet.email)
+end
+
+10.times do
+  category = Category.create!(name: Faker::LeagueOfLegends.summoner_spell)
+  end
+  
+10.times do
+article = Article.create!(title: Faker::Pokemon.name, content: Faker::LeagueOfLegends.champion, user_id: rand(10), category_id: rand(10))
+end
+
+10.times do
+  comment = Comment.create!(content: Faker::FamilyGuy.quote, user_id: 4, article_id: 6)
+end
